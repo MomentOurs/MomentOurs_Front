df896433dc09f3874b35f95d09077f6b00569c9c
JoJeHuni <cho9759@gmail.com>
2025-03-07 18:48:23 +0900

    setup : js 파일 tsx 파일로 확장자명 변경 및 프로젝트 세팅 수정

---
+++ pages/calendar/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CalendarContainer from "../../src/screens/calendar/calendar-container";
+
+const CalendarScreen = () => {
+  return (
+      <CalendarContainer />
+  );
+}
+
+export default CalendarScreen;
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import HomeContainer from "../../src/screens/home/home-container";
 
 const HomeScreen = () => {
