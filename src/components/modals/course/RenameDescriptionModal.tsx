d383e99bb290e2989f7d395c397b48756b0470e5
JoJeHuni <cho9759@gmail.com>
2025-03-10 19:28:33 +0900

    feat : 코스 Modal 추가

---
+++ src/components/modals/course/RenameDescriptionModal.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+interface DescriptionModalProps {
+    visible: boolean;
+    descriptionText: string;
+    onChangeText: (text: string) => void;
+    onClose: () => void;
+    onConfirm: () => void;
+}
+
+const RenameDescriptionModal: React.FC<DescriptionModalProps> = ({ visible, descriptionText, onChangeText, onClose, onConfirm }) => {
+    return (
+        <Modal visible={visible} transparent animationType="fade">
+            <View style={styles.modalOverlay}>
+                <View style={styles.modalContainer}>
+                    <Text style={styles.modalTitle}>설명 변경</Text>
+                    <TextInput 
+                        style={styles.modalInput} 
+                        value={descriptionText} 
+                        onChangeText={onChangeText} 
+                        placeholder="변경할 설명을 입력해 주세요." 
+                    />
+                    <View style={styles.modalButtonContainer}>
+                        <TouchableOpacity style={styles.modalCancelButton} onPress={onClose}>
+                            <Text style={styles.modalCancelText}>취소</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.modalConfirmButton} onPress={onConfirm}>
+                            <Text style={styles.modalConfirmText}>확인</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    modalContainer: {
+        backgroundColor: '#FFFFFF',
+        padding: 20,
+        borderRadius: 10,
+        width: '85%',
+        alignItems: 'center',
+    },
+    modalTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    modalInput: {
+        width: '100%',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 5,
+        padding: 10,
+        fontSize: 16,
+        marginBottom: 15,
+    },
+    modalButtonContainer: {
+        flexDirection: 'row',
+    },
+    modalCancelButton: {
+        padding: 10,
+        marginRight: 10,
+    },
+    modalCancelText: {
+        color: '#888',
+    },
+    modalConfirmButton: {
+        padding: 10,
+        backgroundColor: '#FF6F61',
+        borderRadius: 5,
+    },
+    modalConfirmText: {
+        color: '#FFF',
+    },
+});
+
+export default RenameDescriptionModal;
